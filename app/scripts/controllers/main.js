5cb2b196e78732acecfdfeacd90e2dd2b5154fe3
Daniel Flower <git@danielflower.com>
2013-03-26 23:10:07 +0800

    Trying to make the add button work. Can't get the values from the dynamically generated input boxes though.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,11 +5,19 @@ angular.module('pointsyApp')
 
       $scope.players = [ "Whawha", "Daniel"];
 
+      $scope.playerCount = function () {
+        return $scope.players.length;
+      };
+
       $scope.rounds = [
-          [10, 5],
-          [10, 6],
-          [15, 8]
+        { number: 1, scores: [ 10, 5] },
+        { number: 2, scores: [ 10, 6] },
+        { number: 3, scores: [ 15, 8] }
       ];
+
+      $scope.saveRoundScore = function () {
+        console.log($scope, "scope: change: ", $scope.changeAmount);
+      };
 
 
     });
