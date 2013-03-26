73e6668b628d95894642f88289146b01e8205392
Daniel Flower <git@danielflower.com>
2013-03-26 22:15:56 +0800

    Added some hard-coded scores

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('pointsyApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+
+      $scope.players = [ "Whawha", "Daniel"];
+
+      $scope.rounds = [
+          [10, 5],
+          [10, 6],
+          [15, 8]
+      ];
+
+
+    });
