327d6d0b6bc4fe99679bbf7e9d97eb48822e70c9
Daniel Flower <git@danielflower.com>
2013-04-07 13:49:04 +0800

    Change the delta buttons based on that player's previous performance

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,6 +26,37 @@ pointsy.Game = function () {
     round.isLast = function () {
       return round.number === me.rounds.length;
     };
+    round.commonDeltas = function (scoreIndex) {
+      var lastScore = { index: -1, total: 0 };
+      var deltas = [];
+      var startIndex = Math.max(0, me.rounds.length - 4);
+      for (var i = startIndex; i < me.rounds.length; i++) {
+        var round = me.rounds[i];
+        var score = round.scores[scoreIndex];
+        if (score.total == null) {
+          continue;
+        }
+        var delta = score.total - lastScore.total;
+        if (deltas.indexOf(delta) == -1) {
+          deltas.push(delta);
+        }
+        lastScore = score;
+      }
+
+      if (deltas.length == 0) {
+        deltas = [-1, 0, 1];
+      } else if (deltas.length == 1) {
+        if (deltas.indexOf(0) == -1) {
+          deltas.push(0);
+        } else if (deltas.indexOf(1) == -1) {
+          deltas.push(1);
+        }
+      } else {
+      }
+      deltas.sort();
+
+      return deltas;
+    };
   };
   this.setScore = function (roundIndex, playerIndex, score) {
     while (roundIndex >= this.rounds.length) {
@@ -61,10 +92,6 @@ angular.module('pointsyApp')
       var game = new pointsy.Game();
       game.addPlayer("Whawha");
       game.addPlayer("Daniel");
-      game.setScore(0, 0, 10);
-      game.setScore(0, 1, 15);
-      game.setScore(1, 0, 10);
-      game.setScore(1, 1, 25);
       game.addRound();
 
       $scope.players = game.players;
@@ -102,4 +129,4 @@ angular.module('pointsyApp').
       return function (input) {
         return input > 0 ? "+" + input : input;
       };
-    })
+    });
