eac51630e89ed83df82ea3b66de10f00c6b0bbe9
Daniel Flower <git@danielflower.com>
2013-04-20 21:45:01 +0100

    Rolled back the S3 uploading and updated some tests.

--- test/spec/services/game-factory.js
+++ test/spec/services/game-factory.js
@@ -14,10 +14,26 @@ describe('Service: gameFactory', function () {
     game = _gameFactory_.create();
   }));
 
-  it('should do create a game with no rounds and no players', function () {
+  it('creates a game with no rounds and no players', function () {
     expect(game.rounds.length).toBe(0);
     expect(game.players.length).toBe(0);
+  });
 
+  function addWhaWhaAndDaniel() {
+    game.addPlayer('WhaWha');
+    game.addPlayer('Daniel');
+  }
+
+  it('saves player names to an array', function () {
+    addWhaWhaAndDaniel();
+    expect(game.players).toEqual(['WhaWha', 'Daniel']);
+  });
+
+  it('creates an object to hold scores for each player', function () {
+    addWhaWhaAndDaniel();
+    game.addRound();
+    expect(game.rounds[0].number).toEqual(1);
+    expect(game.rounds[0].scores).toEqual([{index: 0, total: null}, {index: 1, total: null}]);
   });
 
 });
