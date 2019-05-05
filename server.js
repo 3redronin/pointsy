814621a982458badecc14191176ed4a1ebb12f02
Daniel Flower <git@danielflower.com>
2019-05-05 20:30:06 +0800

    Made it apprunner-ready

---
+++ server.js
@@ -0,0 +1,13 @@
+
+var port = parseInt(process.env['APP_PORT'] || '8082');
+var appName = process.env['APP_NAME'] || 'pointsy'; // everything must be served from /{appname}/
+
+var express = require('express');
+var app = express();
+
+
+app.use('/' + appName, express.static('static'));
+
+app.listen(port, 'localhost', function () {
+    console.log('Service started at http://localhost:' + port + '/' + appName + '/');
+});
